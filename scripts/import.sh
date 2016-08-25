5df51832241a52a145bf4c64e9418f7c1dca11bc
Matt Weber <matt@thewebers.ws>
2016-08-25 15:06:35 +0000

    working import function

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl -v -u mweber:cegFifHytch3 -H Expect: -F uploadedfile=$1 -F uploadsubmit=1 http://autobuild.buildroot.org/submit/
+cp $1 /tmp/results.tar.bz2
+curl -v -u mweber:cegFifHytch3 -H Expect: -F uploadedfile=@/tmp/results.tar.bz2 -F uploadsubmit=1 http://autobuild.buildroot.org/submit/
