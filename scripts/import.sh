dbb83f639264d95258b6f918965fc27a535d65ff
Matt Weber <matt@thewebers.ws>
2016-08-27 01:38:44 +0000

    import: default to importing of all files

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-cp $1 /tmp/results.tar.bz2
-curl -v -u mweber:cegFifHytch3 -H Expect: -F uploadedfile=@/tmp/results.tar.bz2 -F uploadsubmit=1 http://autobuild.buildroot.org/submit/
-rm $1
+for i in $(find -name instance-$1-* -prune); do
+	echo "######   Processing [$i]             ######"
+	cp $i /tmp/results.tar.bz2
+	curl -v -u mweber:cegFifHytch3 -H Expect: -F uploadedfile=@/tmp/results.tar.bz2 -F uploadsubmit=1 http://autobuild.buildroot.org/submit/
+	echo "######   Removing tmp files          ######"
+	rm $i /tmp/results.tar.bz2
+	echo "######   Completed Processing [$i]   ######"
+done
