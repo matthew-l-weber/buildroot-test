bd1a9a5cc9f7c2c6c45b56d3777f8bea23096bb7
Matt Weber <matt@thewebers.ws>
2016-08-27 10:56:46 +0000

    view  and import: update find to have explicit string for search

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in $(find -name instance-$1-* -prune); do
+for i in $(find -name "instance-$1-*" -prune); do
 	echo "######   Processing [$i]             ######"
 	cp $i /tmp/results.tar.bz2
 	curl -v -u mweber:cegFifHytch3 -H Expect: -F uploadedfile=@/tmp/results.tar.bz2 -F uploadsubmit=1 http://autobuild.buildroot.org/submit/
--- scripts/view.sh
+++ scripts/view.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in $(find -name instance-$1-* -prune); do
+for i in $(find -name "instance-$1-*" -prune); do
 	echo "######   Extracting [$i]             ######"
 	tar xf $i -C /tmp/
 	cat /tmp/results/build-end.log
