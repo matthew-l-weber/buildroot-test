e79d166f0cceaf6785b1249e4b65cfe44085c09a
Matt Weber <matt@thewebers.ws>
2016-08-28 13:16:00 +0000

    view: delay to view end of log and note completed hash

--- scripts/view.sh
+++ scripts/view.sh
@@ -3,7 +3,9 @@ for i in $(find -name "instance-$1-*" -prune); do
 	echo "######   Extracting [$i]             ######"
 	tar xf $i -C /tmp/
 	cat /tmp/results/build-end.log
+	sleep 2
 	cat /tmp/results/defconfig
+	echo "######   Completed [$i]              ######"
 	echo "Press enter to continue...."
 	read foo
 done
